7e85ec3753bcda321e8605395d1f23882ca0e2f1
Thibaut Rey <thibautrey@me.com>
2026-03-07 09:37:14 +0100

    refactor: normalize documentation links to root-level paths

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/getting-started');
+  redirect('/docs/getting-started');
 }
--- docs/lib/source.ts
+++ docs/lib/source.ts
@@ -2,6 +2,5 @@ import { loader } from 'fumadocs-core/source';
 import { docs } from '../.source/server';
 
 export const source = loader({
-  baseUrl: '/docs',
   source: docs.toFumadocsSource(),
 });
